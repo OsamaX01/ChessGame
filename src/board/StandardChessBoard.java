5394709911a5cd959ea231840b53c96db499e237
OsamaX01 <shkukanios@gmail.com>
2022-08-21 23:44:16 +0300

    implement initializeBoardWithPieces function in StandardChessBoard class

--- src/board/StandardChessBoard.java
+++ src/board/StandardChessBoard.java
@@ -2,6 +2,12 @@ package board;
 
 import general.Location;
 import general.Player;
+import pieces.King;
+import pieces.Queen;
+import pieces.Knight;
+import pieces.Bishop;
+import pieces.Rock;
+import pieces.Pawn;
 
 public class StandardChessBoard extends Board {
     public StandardChessBoard() {
@@ -9,7 +15,26 @@ public class StandardChessBoard extends Board {
     }
 
     @Override
-    public void initializeBoardWithPieces() {
+    public void initializeBoardWithPieces(Player white, Player black) {
+        initializeOneBoardSideWithPieces(white, 0, 1);
+        initializeOneBoardSideWithPieces(black, 7, 6);
+    }
 
+    private void initializeOneBoardSideWithPieces(Player player, int mainRow, int pawnsRow) {
+        addPiece(new Rock(player, new Location(mainRow, 0)));
+        addPiece(new Rock(player, new Location(mainRow, 7)));
+
+        addPiece(new Knight(player, new Location(mainRow, 1)));
+        addPiece(new Knight(player, new Location(mainRow, 6)));
+
+        addPiece(new Bishop(player, new Location(mainRow, 2)));
+        addPiece(new Bishop(player, new Location(mainRow, 5)));
+
+        addPiece(new Queen(player, new Location(mainRow, 3)));
+        addPiece(new King(player, new Location(mainRow, 4)));
+
+        for (int i = mainRow; i < getColumn(); i++) {
+            addPiece(new Pawn(player, new Location(pawnsRow, i)));
+        }
     }
 }
