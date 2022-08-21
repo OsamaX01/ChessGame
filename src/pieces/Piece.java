5e0df4018133a2be604684d0483a692e06666dc9
OsamaX01 <shkukanios@gmail.com>
2022-08-21 19:42:01 +0300

    add movemnts attribute to Pice class added some exception handling to the same class

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -2,23 +2,37 @@ package pieces;
 
 import general.Location;
 import general.Player;
+import movements.MoveStrategy;
+
+import java.util.ArrayList;
 
 abstract public class Piece {
     private Player owner;
     private Location location;
     private final boolean isFirsMove;
+    private ArrayList<MoveStrategy> movements;
 
     public Piece(Player owner, Location location) {
+        if (owner == null || location == null) {
+            throw new NullPointerException();
+        }
         this.owner = owner;
         this.location = location;
         isFirsMove = true;
+        movements = new ArrayList<>();
     }
 
     public void setOwner(Player owner) {
+        if (owner == null) {
+            throw new NullPointerException();
+        }
         this.owner = owner;
     }
 
     public void setLocation(Location location) {
+        if (location == null) {
+            throw new NullPointerException();
+        }
         this.location = location;
     }
 
@@ -32,5 +46,31 @@ abstract public class Piece {
 
     public boolean isFirsMove() {
         return isFirsMove;
+    }
+
+    public void addMovementStrategy(MoveStrategy movement) {
+        if (movement == null) {
+            throw new NullPointerException();
+        }
+        if (movements.contains(movement)) {
+            System.out.println("this movement is already added! ");
+            return;
+        }
+        movements.add(movement);
+    }
+
+    public void removeMovementStrategy(MoveStrategy movement) {
+        if (movement == null) {
+            throw new NullPointerException();
+        }
+        if (!movements.contains(movement)) {
+            System.out.println("does not contain this movement to remove! ");
+            return;
+        }
+        movements.remove(movement);
+    }
+
+    public ArrayList<MoveStrategy> getMovements() {
+        return movements;
     }
 }
