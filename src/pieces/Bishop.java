1d4a4e7c6063c9cc73b721810d05e6b26787edb8
OsamaX01 <shkukanios@gmail.com>
2022-08-21 23:00:19 +0300

    add main chess pieces classes

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -3,13 +3,13 @@ package pieces;
 import general.Location;
 import general.Player;
 
-public class Pawn extends Piece {
-    public Pawn(Player owner, Location location) {
+public class Bishop extends Piece {
+    public Bishop(Player owner, Location location) {
         super(owner, location);
     }
 
     @Override
     public String getName() {
-        return "Pawn";
+        return "Bishop";
     }
 }
--- src/pieces/King.java
+++ src/pieces/King.java
@@ -3,13 +3,13 @@ package pieces;
 import general.Location;
 import general.Player;
 
-public class Pawn extends Piece {
-    public Pawn(Player owner, Location location) {
+public class King extends Piece {
+    public King(Player owner, Location location) {
         super(owner, location);
     }
 
     @Override
     public String getName() {
-        return "Pawn";
+        return "King";
     }
 }
