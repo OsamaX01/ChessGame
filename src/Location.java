988ce6a9fabf6262421031d2d89d2a5e70b011c5
OsamaX01 <shkukanios@gmail.com>
2022-08-21 07:57:32 +0300

    add Location class

---
+++ src/Location.java
@@ -0,0 +1,26 @@
+import javafx.util.Pair;
+
+public class Location extends Pair<Integer, Integer> {
+
+    public Location(Integer row, Integer column) {
+        super(row, column);
+    }
+
+    public int getRow() {
+        return getKey();
+    }
+
+    public int getColumn() {
+        return getValue();
+    }
+
+    @Override
+    public String toString() {
+        return "Location : " + getRow() + ", " + getColumn();
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        return super.equals(rhs);
+    }
+}
