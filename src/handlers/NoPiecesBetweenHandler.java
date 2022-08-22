f274a20756d950bff86d760111a87d6a948f09bb
OsamaX01 <shkukanios@gmail.com>
2022-08-22 15:25:54 +0300

    add NoPiecesBetweenHandler class

--- src/handlers/NoPiecesBetweenHandler.java
+++ src/handlers/NoPiecesBetweenHandler.java
@@ -1,2 +1,32 @@
-package handlers;public class NoPiecesBetweenHandler {
+package handlers;
+
+import board.Board;
+import general.Location;
+
+public class NoPiecesBetweenHandler extends BaseHandler {
+
+    @Override
+    protected boolean isValid(Board board, Location from, Location to) {
+        int fromRow = from.getRow();
+        int fromColumn = from.getColumn();
+        while (fromRow != to.getRow() || fromColumn != to.getColumn()) {
+            fromRow += getCloser(fromRow, to.getRow());
+            fromColumn += getCloser(fromColumn, to.getColumn());
+            if (board.getPieceAt(new Location(fromRow, fromColumn)) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getCloser(int from, int to) {
+        int differance = from - to;
+        if (differance == 0) {
+            return 0;
+        } else if (differance < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
 }
