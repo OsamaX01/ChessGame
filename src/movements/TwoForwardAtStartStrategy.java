3cfc9351576568cc237b3a7cacb002e7960edaae
OsamaX01 <shkukanios@gmail.com>
2022-08-22 21:02:45 +0300

    add TwoForwardAtStartStrategy class

--- src/movements/TwoForwardAtStartStrategy.java
+++ src/movements/TwoForwardAtStartStrategy.java
@@ -1,2 +1,25 @@
-package movements;public class TwoForwardAtStartStrategy {
+package movements;
+
+import board.Board;
+import general.Location;
+import handlers.*;
+
+public class TwoForwardAtStartStrategy implements MoveStrategy {
+    @Override
+    public boolean validateMove(Board board, Location from, Location to) {
+        BaseHandler handler1 = new NullPointerHandler();
+        BaseHandler handler2 = new ValidDestinationHandler();
+        BaseHandler handler3 = new AtTheFirstMoveHandler();
+        BaseHandler handler4 = new TwoMovesForwardHandler();
+        BaseHandler handler5 = new NoPiecesBetweenHandler();
+        BaseHandler handler6 = new EmptyDestinationHandler();
+
+        handler1.setNext(handler2);
+        handler2.setNext(handler3);
+        handler3.setNext(handler4);
+        handler4.setNext(handler5);
+        handler5.setNext(handler6);
+
+        return handler1.canHandle(board, from, to);
+    }
 }
