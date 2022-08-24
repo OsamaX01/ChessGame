156c9e9fca696c2044a3c9aca272c074a3ad5f4e
OsamaX01 <shkukanios@gmail.com>
2022-08-24 18:27:07 +0300

    modify OneMoveForwardHandler and TwoMovesForwardHandler to handle Black pieces correctly

--- src/handlers/OneMoveForwardHandler.java
+++ src/handlers/OneMoveForwardHandler.java
@@ -1,11 +1,15 @@
 package handlers;
 
 import board.Board;
+import general.Color;
 import general.Location;
 
 public class OneMoveForwardHandler extends BaseHandler {
     @Override
     protected boolean isValid(Board board, Location from, Location to) {
+        if (board.getPieceAt(from).getOwner().getColor() == Color.BLACK) {
+            return to.getRow() == from.getRow() - 1;
+        }
         return to.getRow() == from.getRow() + 1;
     }
 }
--- src/handlers/TwoMovesForwardHandler.java
+++ src/handlers/TwoMovesForwardHandler.java
@@ -1,6 +1,7 @@
 package handlers;
 
 import board.Board;
+import general.Color;
 import general.Location;
 
 import board.Board;
@@ -9,6 +10,9 @@ import general.Location;
 public class TwoMovesForwardHandler extends BaseHandler {
     @Override
     protected boolean isValid(Board board, Location from, Location to) {
+        if (board.getPieceAt(from).getOwner().getColor() == Color.BLACK) {
+            return to.getRow() == from.getRow() - 2;
+        }
         return to.getRow() == from.getRow() + 2;
     }
 }
